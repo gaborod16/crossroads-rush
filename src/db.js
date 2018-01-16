f881e60cda229c5c81d91c6b638224c3d982d603
Gabriel Amarista <gaborod16@gmail.com>
2018-01-16 02:52:25 +0000

    Set up mlab mongodb

--- src/db.js
+++ src/db.js
@@ -1,3 +1,3 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/cross_rush');
-
+// mongoose.connect('mongodb://localhost:27017/cross_rush');
+mongoose.connect('mongodb://admin:admin@ds251747.mlab.com:51747/cross_rush');
